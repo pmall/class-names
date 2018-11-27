d4fc6fee77f270e49ed69276d54498c90615f0e7
pmall <pierre.mallinjoud@gmail.com>
2018-11-27 20:16:40 +0100

    add classes

---
+++ src/Psr4Namespace.php
@@ -0,0 +1,100 @@
+<?php declare(strict_types=1);
+
+namespace Quanta\Utils;
+
+final class Psr4Namespace
+{
+    /**
+     * The pattern a file name must match to be considered as a file containing
+     * a class declaration.
+     *
+     * Starts with an uppercased letter, contains only letters an numbers and
+     * have php extension.
+     *
+     * It actually also matches interfaces and traits but who cares.
+     *
+     * @var string
+     */
+    const PATTERN = '/[A-Z][A-Za-z0-9]+\.php$/';
+
+    /**
+     * The namespace.
+     *
+     * @var string
+     */
+    private $namespace;
+
+    /**
+     * The directories containing the php files declaring the namespace classes.
+     *
+     * @var string[]
+     */
+    private $directories;
+
+    /**
+     * Constructor.
+     *
+     * At least one directory is required.
+     *
+     * @param string $namespace
+     * @param string $directory
+     * @param string ...$directories
+     */
+    public function __construct(string $namespace, string $directory, string ...$directories)
+    {
+        $this->namespace = $namespace;
+        $this->directories = array_merge([$directory], $directories);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function classes(): array
+    {
+        return iterator_to_array($this->fqcn());
+    }
+
+    /**
+     * Yield an iterator for each directory.
+     *
+     * @return \Generator
+     */
+    private function directories(): \Generator
+    {
+        foreach ($this->directories as $root) {
+            yield $root => new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($root)
+            );
+        }
+    }
+
+    /**
+     * Yield paths of the files declaring classes for each directory.
+     *
+     * @return \Generator
+     */
+    private function paths(): \Generator
+    {
+        foreach ($this->directories() as $root => $directory) {
+            foreach ($directory as $file) {
+                if (preg_match(self::PATTERN, $file->getFilename()) === 1) {
+                    yield $root => $file->getPathname();
+                }
+            }
+        }
+    }
+
+    /**
+     * Yield the fully qualified class name of the namespace classes.
+     *
+     * @return \Generator
+     */
+    private function fqcn(): \Generator
+    {
+        foreach ($this->paths() as $root => $path) {
+            $x = substr($path, strlen($root) + 1, -4);
+
+            yield $path => $this->namespace . str_replace('/', '\\', $x);
+        }
+    }
+}
---
+++ src/VendorDirectory.php
@@ -0,0 +1,93 @@
+<?php declare(strict_types=1);
+
+namespace Quanta\Utils;
+
+final class VendorDirectory
+{
+    /**
+     * The vendor dir path.
+     *
+     * @var string
+     */
+    private $path;
+
+    /**
+     * Constructor.
+     *
+     * @param string $path
+     */
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function classes(): array
+    {
+        return iterator_to_array($this->fqcn());
+    }
+
+    /**
+     * Return whether the given values defines a valid namespace.
+     *
+     * @param mixed $namespace
+     * @param mixed $directories
+     * @return bool
+     */
+    private function isValidNamespace($namespace, $directories): bool
+    {
+        if (is_string($namespace) && is_array($directories)) {
+            $total = count($directories);
+            $valid = count(array_filter($directories, 'is_string'));
+
+            return $total > 0 && $total == $valid;
+        }
+
+        return false;
+    }
+
+    /**
+     * Yield namespace/directories pairs from the psr4 autoload file.
+     *
+     * @return \Generator
+     */
+    private function map(): \Generator
+    {
+        $path = realpath($this->path) . '/composer/autoload_psr4.php';
+
+        if (file_exists($path) && is_array($map = require $path)) {
+            yield from $map;
+        }
+    }
+
+    /**
+     * Yield an instance of Psr4Namespace for each entry of the autoload map.
+     *
+     * @return \Generator
+     */
+    private function namespaces(): \Generator
+    {
+        foreach ($this->map() as $namespace => $directories) {
+            if ($this->isValidNamespace($namespace, $directories)) {
+                yield new Psr4Namespace($namespace, ...$directories);
+            }
+        }
+    }
+
+    /**
+     * Yield the fully qualified class names of the classes defined for each
+     * namespace.
+     *
+     * @return \Generator
+     */
+    private function fqcn(): \Generator
+    {
+        foreach ($this->namespaces() as $namespace) {
+            foreach ($namespace->classes() as $class) {
+                yield $class;
+            }
+        }
+    }
+}
